8b959e26c9b85e76d00f12a9d289d0c9ad03ca87
Eliot Quon <eliot.quon@nrel.gov>
2019-09-10 00:41:28 -0600

    Rename "YATLA" to "YATL"

--- yatla-cli.py
+++ yatla-cli.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
+#
+# Yet Another Todo List Application
+# (command line interface)
+#
 import os
-from yatla.todo import Todo
+from yatl.todo import Todo
 
 debug = True
 
 try:
-    YATLA_PATH = os.environ['YATLA_PATH']
+    YATL_PATH = os.environ['YATL_PATH']
 except KeyError:
-    YATLA_PATH = os.environ['HOME']
+    YATL_PATH = os.environ['HOME']
 finally:
-    if os.path.isdir(YATLA_PATH):
-        YATLA_PATH = os.path.join(YATLA_PATH, 'yet_another_todo.list')
+    if os.path.isdir(YATL_PATH):
+        YATL_PATH = os.path.join(YATL_PATH, 'yet_another_todo.list')
 if debug:
-    print('todo list:',YATLA_PATH)
+    print('todo list:',YATL_PATH)
 
 #
 # Start here
 #
 if __name__ == '__main__':
-    todo = Todo(YATLA_PATH)
+    todo = Todo(YATL_PATH)
     #todo.add_task('Email Jeff re FY20 AOP', importance=3, cost=1)
     todo.plot()
