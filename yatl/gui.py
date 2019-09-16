e234c164f55a51bc9f8e150a1fae451a74c983c6
Eliot Quon <eliot.quon@nrel.gov>
2019-09-15 22:34:00 -0600

    Implement new task part of gui

--- yatl/gui.py
+++ yatl/gui.py
@@ -100,12 +100,74 @@ class TaskList(tk.Frame):
         return values
 
 
+class TaskCreator(tk.Frame):
+    default_description = 'Enter new task description here...'
+
+    def __init__(self, parent, todo, **kwargs):
+        """Create a series of controls for adding new tasks"""
+        tk.Frame.__init__(self, parent, **kwargs)
+        self.todo = todo
+        value_minmax = todo.value_minmax
+        print(id(self.todo.df))
+        # variables
+        self.task_description = tk.StringVar(value=self.default_description)
+        self.importance = tk.DoubleVar(value=value_minmax[1])
+        self.cost = tk.DoubleVar(value=value_minmax[0])
+        # controls
+        self.description_entry = tk.Entry(self,
+                                          width=default_task_charlen,
+                                          textvariable=self.task_description)
+        self.importance_label = tk.Label(self, text='importance', anchor='s')
+        self.importance_ctrl = tk.Scale(self,
+                                        orient='horizontal', length=200,
+                                        variable=self.importance,
+                                        from_=value_minmax[0],
+                                        to=value_minmax[1],
+                                        resolution=1.0)
+        self.cost_label = tk.Label(self, text='cost', anchor='s')
+        self.cost_ctrl = tk.Scale(self,
+                                  orient='horizontal', length=200,
+                                  variable=self.cost,
+                                  from_=value_minmax[0],
+                                  to=value_minmax[1],
+                                  resolution=1.0)
+        bg = self.cget("background") # system-specific
+        self.spacer = tk.Frame(self, height=10, bg=bg)
+        self.add_task_button = tk.Button(self,
+                                         text='add task',
+                                         fg='blue',
+                                         padx=20, pady=5,
+                                         command=self.add_task)
+        # create layout
+        self.description_entry.pack()
+        self.importance_label.pack()
+        self.importance_ctrl.pack()
+        self.cost_label.pack()
+        self.cost_ctrl.pack()
+        self.spacer.pack()
+        self.add_task_button.pack()
+
+    def add_task(self):
+        description = self.description_entry.get()
+        importance = self.importance.get()
+        cost = self.cost.get()
+        priority = importance / cost
+        print(description, importance, cost, priority)
+        self.description_entry.delete(0, tk.END)
+        self.description_entry.insert(0, self.default_description)
+
+
 class YATLApp(object):
     def __init__(self, master, todo):
         self.master = master
         self.todo = todo
         self.todolist = TaskList(master, self.todo.df)
+        self.taskctrl = TaskCreator(master, self.todo)
         self.todolist.pack()
+        self.taskctrl.pack(side='left')
+
+#    def onclose(self):
+#        # TODO: prompt to copy temp todo over master todo
 
 
 if __name__ == '__main__':
